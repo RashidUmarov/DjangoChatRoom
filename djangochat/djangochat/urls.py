9b84b0c978dc3b7be0b2517837b6d0a18b15cd10
Rashid Umarov <rasrasras2014@mail.ru>
2023-04-06 22:13:06 +0300

    added Swagger schemas and url http://127.0.0.1:8000/swagger-ui/

--- djangochat/djangochat/urls.py
+++ djangochat/djangochat/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chat.urls'))
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
+    path('', include('chat.urls')),
 ]
